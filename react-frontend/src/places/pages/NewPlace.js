bde310e9840ea25fefe52b6a01e5cca8e901049e
Rajesh Kumar <rkumar125787@gmail.com>
2021-09-02 11:56:08 +0530

    initial react-router-implemented

---
+++ react-frontend/src/places/pages/NewPlace.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const NewPlace = () => {
+    return <React.Fragment>
+        <h2>NewpLace Works</h2>
+    </React.Fragment>
+}
+export default NewPlace
---
+++ react-frontend/src/user/pages/Users.js
@@ -0,0 +1,6 @@
+import React from 'react';
+const Users = () => {
+    return <h2>Users Welcome</h2>
+}
+
+export default Users;
